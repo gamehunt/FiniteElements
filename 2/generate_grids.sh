f707ae3b586675c63496b1449b07e76613053af8
Lizord <mt.demakov@gmail.com>
2026-04-06 16:39:45 +0300

    a

--- 2/generate_grids.sh
+++ 2/generate_grids.sh
@@ -4,5 +4,10 @@ set -e
 grids=(5 10 20)
 
 for g in "${grids[@]}"; do
-	gmsh grid_${g}.geo -2 grid_${g}.msh
+	name=grid_${g}
+	par=mesh/$g
+	mkdir -p $par
+	sed "s/%SIZE%/${g}/g" grid_template.geo > $par/$name.geo
+	gmsh $par/${name}.geo -2 -format msh2 $par/${name}.msh
+	dolfin-convert $par/${name}.msh $par/${name}.xml
 done
---
+++ 2/solve.py
@@ -0,0 +1,38 @@
+from fenics import *
+import matplotlib.pyplot as plt
+
+mesh = Mesh("mesh/5/grid_5.xml")
+boundaries = MeshFunction("size_t", mesh, "mesh/5/grid_5_facet_region.xml")
+
+ds = Measure("ds", subdomain_data=boundaries)
+
+  
+V = FunctionSpace(mesh, "CG", 2)
+
+# Условие на входе в канал
+u_1 = Expression("x[1]", degree=2)
+
+# Граничные условия
+bcs = [DirichletBC(V, Constant(0.0), boundaries, 1),
+       DirichletBC(V, Constant(1.0), boundaries, 2),
+       DirichletBC(V, Constant(0.5), boundaries, 5),
+       DirichletBC(V, u_1, boundaries, 3)]
+
+# Вариационная задача
+u = TrialFunction(V)
+v = TestFunction(V)
+f = Constant(0.0)  
+a = dot(grad(u), grad(v)) * dx
+L = f * v * dx
+
+# Решение задачи
+u = Function(V)
+solve(a == L, u, bcs)
+plot(u, title="Решение")
+plt.show()
+
+#  Циркуляция 
+n = FacetNormal(mesh)  
+u_n = dot(grad(u), n)  
+Gamma = assemble(u_n * ds(subdomain_data=boundaries, subdomain_id=5))
+print(Gamma)
