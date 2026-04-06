cbb47f431524292ed836682adff561e895ba32f7
surious-type <daniil_vasilenko757@mail.ru>
2026-04-06 20:22:57 +0300

    fix: правки по ревью

--- 2/generate_grids.sh
+++ 2/generate_grids.sh
@@ -10,7 +10,7 @@ for g in "${grids[@]}"; do
     name=grid_${g}
     par=mesh/$g
     mkdir -p $par
-    sed -e "s/%SIZE%/${g}/g" -e "s/%LENGHT%/${l_base}/g" -e "s/%HEIGHT%/${h_base}/g" grid_template.geo >$par/$name.geo
+    sed -e "s/%SIZE%/${g}/g" -e "s/%LENGTH%/${l_base}/g" -e "s/%HEIGHT%/${h_base}/g" grid_template.geo >$par/$name.geo
     gmsh $par/${name}.geo -2 -format msh2 $par/${name}.msh
     dolfin-convert $par/${name}.msh $par/${name}.xml
 done
@@ -21,7 +21,7 @@ for l in "${lenghts[@]}"; do
     name=grid_l_${l}
     par=mesh/$name
     mkdir -p $par
-    sed -e "s/%SIZE%/${size_base}/g" -e "s/%LENGHT%/${l}/g" -e "s/%HEIGHT%/${h_base}/g" grid_template.geo >$par/$name.geo
+    sed -e "s/%SIZE%/${size_base}/g" -e "s/%LENGTH%/${l}/g" -e "s/%HEIGHT%/${h_base}/g" grid_template.geo >$par/$name.geo
     gmsh $par/${name}.geo -2 -format msh2 $par/${name}.msh
     dolfin-convert $par/${name}.msh $par/${name}.xml
 done
@@ -32,7 +32,7 @@ for h in "${heights[@]}"; do
     name=grid_h_${h}
     par=mesh/$name
     mkdir -p $par
-    sed -e "s/%SIZE%/${size_base}/g" -e "s/%LENGHT%/${l_base}/g" -e "s/%HEIGHT%/${h}/g" grid_template.geo >$par/$name.geo
+    sed -e "s/%SIZE%/${size_base}/g" -e "s/%LENGTH%/${l_base}/g" -e "s/%HEIGHT%/${h}/g" grid_template.geo >$par/$name.geo
     gmsh $par/${name}.geo -2 -format msh2 $par/${name}.msh
     dolfin-convert $par/${name}.msh $par/${name}.xml
 done
